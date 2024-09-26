666fec45d28078a406b0b0370b37b610580855cd
mario.calvillo <mario.calvillo@auleseebe.upc.edu>
2024-09-26 11:11:13 +0200

    ..

--- Problema2.R
+++ Problema2.R
@@ -1,1 +1,39 @@
 #Codigo para problema 2
+
+iris
+mis_dades <- iris;
+y <- mis_dades$Sepal.Length
+y
+
+x <- mis_dades$Petal.Length
+plot(x,y)
+
+xbar <- mean(x)
+ybar <- mean(y)
+
+m <- (sum((x-xbar)*(y-ybar)))/sum((x-xbar)^2)
+m
+
+b <- ybar - (m*xbar)
+b
+
+a = (m*1.5) + b
+a
+
+mod <- lm(y~x)
+mod
+
+ypredicted <- predict(mod,data.frame(x=x))
+ypredicted
+
+plot(x,y)
+lines(x,ypredicted)
+
+
+Rsq <- sum((ypredicted-ybar)^2)/sum((y-ybar)^2)
+Rsq
+
+summry(mod)
+sqrt(Rsq)
+cor.test(x,y) #te da e valor de correlacion de tu funcion igual
+
